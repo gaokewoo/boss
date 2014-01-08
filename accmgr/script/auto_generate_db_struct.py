e57c5102eebf89f6e76c5b65d718be2da60989e6
Gaoke Wu <gaokewoo@126.com>
2014-01-08 23:14:53 +0800

    [task] Complete the auto generate program for db

--- accmgr/script/auto_generate_db_struct.py
+++ accmgr/script/auto_generate_db_struct.py
@@ -3,32 +3,52 @@ import os
 import re
 
 path = './db/table_column/'
-
-def handleLine(file,line):
-    line.strip()
-    if (line[0] == "#") or (line[0] == "(") or (line[0]==")" or (line.find("constraint") != -1)):
-        return
-    first_two_data=line.split(None,2)
-    v_name=first_two_data[0].lower()
-    v_type= " ".join(re.findall("[a-zA-Z]+", first_two_data[1])) #Extract only chracters from a string
-    v_type=v_type.lower()
-    print v_name,v_type
+dest_file = "../db/DBStruct.hh"
+dest_file_obj = open(dest_file,"w")
 
 def handleFile(file):
     file_object = open(path+file,'r')
     try:
+        dest_file_obj.write("typedef struct {\n")
         all_the_text = file_object.readlines()
         for line in all_the_text:
-            handleLine(file,line)
+            line=line.strip()
+            #print "My line is:"+line
+            if (line == "") or (line[0] == "#") or (line[0] == "(") or (line[0]==")" or (line.find("constraint") != -1)):
+                continue
+
+            first_two_data=line.split(None,2)
+            #print first_two_data
+            v_name=first_two_data[0].lower()
+            v_name=v_name.strip('"')
+
+            tmp_type= " ".join(re.findall("[a-zA-Z]+", first_two_data[1])) #Extract only chracters from a string
+            tmp_type=tmp_type.lower()
+            if (tmp_type == "number"):
+                v_type="long"
+            else:
+                v_type="string"
+
+            dest_file_obj.write("\t"+v_type+"\tm_"+v_name+";\n")
+        dest_file_obj.write("} ST_"+file+";\n\n")
+
     finally:
         file_object.close()
 
+#main function
 files = os.listdir(path)
+dest_file_obj.write("#ifndef __DBSTRUCT__\n"
+                    "#define __DBSTRUCT__\n"
+                    "#include <string>\n"
+                    "using namespace std;\n\n")
+
 numfiles = 0
 for f in files:
     print "Start to analyse ",f
-    handleFile(f) 
-    numfiles +=1
-    break
-print "A total of [",numfiles,"] files analysed"
+    handleFile(f)
 
+    numfiles +=1
+
+dest_file_obj.write("#endif\n")
+
+print "A total of [",numfiles,"] files analysed"
