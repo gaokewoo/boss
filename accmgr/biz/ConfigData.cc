17e7a2afee27f749081b184b83620a0cd96b0e0c
Gaoke Wu <gaokewoo@126.com>
2014-02-15 20:38:12 +0800

    [task] Make the db connect configurable

--- accmgr/biz/ConfigData.cc
+++ accmgr/biz/ConfigData.cc
@@ -1,11 +1,21 @@
 #include "ConfigData.hh"
+#include "libconfparser/confparser.hpp"
 
 ConfigData::ConfigData(LoggerId logId)
 {
     m_logId = logId;
     LOG_DEBUG(m_logId, "ConfigData::ConfigData start");
 
-    m_db = new OracleDB();
+    LOG_DEBUG(logId, "Parse boss.cfg");
+    CONF_PARSER_SIMPLE_INIT("../conf/boss.cfg");
+    string db_user = CONF_PARSER_GET_VAL("DB", "user");
+    string db_passwd = CONF_PARSER_GET_VAL("DB", "passwd");
+    string db_instance = CONF_PARSER_GET_VAL("DB", "instance");
+    LOG_DEBUG(logId, "DB User:"<<db_user);
+    LOG_DEBUG(logId, "DB Passwd:"<<db_passwd);
+    LOG_DEBUG(logId, "DB Instance:"<<db_instance);
+
+    m_db = new OracleDB(db_user,db_passwd,db_instance);
     m_db->connectToDB();
 
     m_seq.setConnection(m_db->getConnection());
