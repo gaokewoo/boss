a209bc51db019a4a30415cdfe6b3c111e2562959
Gaoke Wu <gaokewoo@126.com>
2014-03-09 22:53:47 +0800

    [task] Add rabbitmq related configuration

--- accmgr/env.sh
+++ accmgr/env.sh
@@ -4,7 +4,7 @@ export ORACLE_BASE=/u01/app/oracle
 export ORACLE_HOME=/u01/app/oracle/product/11.2.0/dbhome_1
 export NLS_LANG=american_america.utf8
 export BOSS_INSTALL_HOME=/opt-boss/
-export PATH=$ORACLE_HOME/bin:$BOSS_INSTALL_HOME/thrift/bin/:/usr/local/bin:/usr/sbin:$PATH
+export PATH=$ORACLE_HOME/bin:$BOSS_INSTALL_HOME/thrift/bin/:$BOSS_INSTALL_HOME/erlang/bin/:$BOSS_INSTALL_HOME/rabbitmq/sbin/:/usr/local/bin:/usr/sbin:$PATH
 export CLASSPATH=$ORACLE_HOME/JRE:$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib:$ORACLE_HOME/jdk/jre/lib/ext
 export SHLIB_PATH=$ORACLE_HOME/lib:$ORACLE_HOME/rdbms/lib:$ORACLE_HOME/lib:.
 
