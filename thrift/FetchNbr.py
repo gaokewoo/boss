cacceedae6a5902e4bd85bd4f3c95768efa2ea50
Gaoke Wu <gaokewoo@126.com>
2014-03-19 16:52:58 +0800

    [task] Update FetchNbr.py file, make it can read ip, port from configuration file

--- thrift/FetchNbr.py
+++ thrift/FetchNbr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python  
-import sys  
-sys.path.append('./gen-py/')
+import sys
+import ConfigParser
+sys.path.append('../thrift/gen-py/')
 from query import FetchNbr
 from query.ttypes import *
 from thrift import Thrift
@@ -8,15 +9,24 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
-# Make socket  
-transport = TSocket.TSocket('localhost', 9093)  
-# Buffering is critical. Raw sockets are very slow  
-transport = TTransport.TBufferedTransport(transport)  
-# Wrap in a protocol  
-protocol = TBinaryProtocol.TBinaryProtocol(transport)  
-# Create a client to use the protocol encoder  
+# Read configuration file
+config = ConfigParser.ConfigParser()
+config.readfp(open("../conf/boss.cfg","rb"))
+
+ip = config.get("FetchNbr","ip")
+port = config.get("FetchNbr","port")
+
+# Make socket
+transport = TSocket.TSocket(ip, port)
+# Buffering is critical. Raw sockets are very slow
+transport = TTransport.TBufferedTransport(transport)
+# Wrap in a protocol
+protocol = TBinaryProtocol.TBinaryProtocol(transport)
+# Create a client to use the protocol encoder
 client = FetchNbr.Client(protocol)
 # Connect!
-transport.open()  
-# Call Server services    
-print "Phone Nbr:"+client.fetchNbr() 
+transport.open()
+# Call Server services
+phoneNbr = client.fetchNbr()
+
+print "Phone Nbr:"+phoneNbr
