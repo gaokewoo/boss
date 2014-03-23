c9df3a2decdedebde87de1ec104bc229f644fa7a
Gaoke Wu <gaokewoo@126.com>
2014-03-23 09:55:49 +0800

    [task] Add tinyxml2 to support parse xml message

--- global.mk
+++ global.mk
@@ -2,7 +2,7 @@ CC_FLAGS= -g3 -Wall -fpic
 CC=g++
 AR_CMD=ar -rv
 
-SUBDIRS=ds db pub biz thrift main
+SUBDIRS=tinyxml2 ds db pub biz thrift main
 
 #define lib path
 BOSS_LIB_PATH=$(BOSS_DEV_HOME)/lib/
@@ -13,11 +13,14 @@ PUB_INCLUDE=$(BOSS_DEV_HOME)/pub/
 BIZ_INCLUDE=$(BOSS_DEV_HOME)/biz/ 
 DS_INCLUDE=$(BOSS_DEV_HOME)/ds/ 
 BOSS_THRIFT_INCLUDE=$(BOSS_DEV_HOME)/thrift/
-BOSS_LIBS=$(BOSS_LIB_PATH)/ -lbossbiz -lbossdb -lbosspub -lbossthrift -lbossalg
+TINYXML2_INCLUDE=$(BOSS_DEV_HOME)/tinyxml2/
+BOSS_LIBS=$(BOSS_LIB_PATH)/ -lbossbiz -lbossdb -lbosspub -lbossthrift -lbossalg -ltinyxml2
 
 #thrift dir
 THRIFT_INCLUDE=$(BOSS_HOME)/thrift/include/
 THRIFT_LIBS=$(BOSS_HOME)/thrift/lib -lthrift
+
+#tinyxml2 dir
 
 #gmock dir
 GMOCK_INCLUDE=$(BOSS_DEV_HOME)/gmock/include/
@@ -35,6 +38,7 @@ CC_INCLDIR=-I. \
 		   -I$(PUB_INCLUDE) \
 		   -I$(BIZ_INCLUDE) \
 		   -I$(DS_INCLUDE) \
+		   -I$(TINYXML2_INCLUDE) \
 		   -I/usr/include
 
 ORA_LIBS=$(ORACLE_HOME)/lib -locci -lclntsh
