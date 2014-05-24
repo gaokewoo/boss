7516288f440f6413a8d890dbedf4813792f3fccf
Gaoke Wu <gaokewoo@126.com>
2014-05-25 02:22:14 +0800

    [task] Make DetailItemMain daemon and only load one FetchNbr object

--- main/DetailItemMain.cc
+++ main/DetailItemMain.cc
@@ -3,6 +3,9 @@
 #include <signal.h>
 #include "libconfparser/confparser.hpp"
 #include "DetailItem.hh"
+#include <queue>
+
+using namespace std;
 
 int frequence;
 LoggerId logId;
@@ -13,14 +16,72 @@ void handler(int signo)
     exit(0);  
 }
 
+class DetailItemPool
+{
+    public:
+        DetailItemPool(LoggerId logId, int poolNum=10)
+        {
+            qready = PTHREAD_COND_INITIALIZER;
+            qlock = PTHREAD_MUTEX_INITIALIZER;
+
+            for(int i=0; i<poolNum; i++)
+            {
+                m_queue.push(new DetailItem(logId));
+            }
+        }
+
+        ~DetailItemPool()
+        {
+            while(!m_queue.empty())
+            {
+                DetailItem *p = m_queue.front();
+                m_queue.pop();
+
+                delete p;
+            }
+        }
+
+        DetailItem* get()
+        {
+            pthread_mutex_lock(&qlock);
+
+            while(m_queue.empty())
+            {
+                pthread_cond_wait(&qready, &qlock);
+            }
+
+            DetailItem *p = m_queue.front();
+            m_queue.pop();
+
+            pthread_mutex_unlock(&qlock);
+
+            return p;
+        }
+
+        void put(DetailItem *p)
+        {
+            pthread_mutex_lock(&qlock);
+            m_queue.push(p);
+            pthread_mutex_unlock(&qlock);
+            pthread_cond_signal(&qready);
+        }
+
+    private:
+        queue<DetailItem*> m_queue;
+        pthread_cond_t qready;
+        pthread_mutex_t qlock;
+};
+
+DetailItemPool *m_detail_item_pool=NULL;
+
 void *thread_func(void *)
 {
-    DetailItem *m_detail_item = new DetailItem(logId);
-
     while(1)
     {
         //cout<<"----------thread_id:"<<pthread_self()<<"["<<++i<<"]----------"<<endl;
-        m_detail_item->doBiz();
+        DetailItem *p = m_detail_item_pool->get();
+        p->doBiz();
+        m_detail_item_pool->put(p);
 
         usleep(frequence*1000);
     }
@@ -28,7 +89,28 @@ void *thread_func(void *)
     return 0;
 }
 
+void mkDaemon(void)
+{
+    pid_t pid;
+    pid_t sid;
+
+    pid = fork();
+    if (pid == -1)
+      printf("Failed to fork() ");
+
+    if (pid) {
+        exit(0);
+    }
+
+    sid = setsid();
+    if (sid == -1)
+      abort();
+    return;
+} 
+
 int main(int argc, char** argv) {   
+
+    mkDaemon();
 
     ILog4zManager::GetInstance()->Config("../conf/log.cfg");
     logId = ILog4zManager::GetInstance()->FindLogger("DetailItem");
@@ -45,6 +127,7 @@ int main(int argc, char** argv) {
 
     try 
     {
+        m_detail_item_pool=new DetailItemPool(logId);
 
         pthread_t *threadArr=new pthread_t[thread_num];
         for(int i=0; i<thread_num; i++)
